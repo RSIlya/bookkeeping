6b811f4096c77b6b6ee5c2081c944e395fc76776
RSIlya <_ris_@mail.ru>
2022-03-14 21:06:30 +0500

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+
+from datetime import datetime
+
+
+def get_employees():
+    print('function call: get_employees', datetime.now())
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+
+from datetime import datetime
+
+
+def calculate_salary():
+    print('function call: calculate_salary', datetime.now())
